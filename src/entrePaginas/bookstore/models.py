17858ce6cbb05d55500ad750980dac329a0b64bf
mramirezcc <mramirezcc@unsa.edu.pe>
2024-07-01 21:51:37 -0500

    Modelos definidos por completo

--- src/entrePaginas/bookstore/models.py
+++ src/entrePaginas/bookstore/models.py
@@ -7,16 +7,25 @@ class User(models.Model):
     address = models.CharField()
 
 class ShoppingCart(models.Model):
+    idUser = models.ForeignKey(User, on_delete = models.CASCADE)
     total = models.IntegerField()
-
-class CartBook(models.Model):
-    idCart = models.ForeignKey(ShoppingCart, on_delete = models.CASCADE)
-
-class Book(models.Model):
-    name = models.CharField()
+    status = models.BooleanField(default = False)
 
 class Cathegory(models.Model):
     name = models.CharField()
+
+class Book(models.Model):
+    name = models.CharField()
+    year = models.IntegerField(max_length = 4)
+    author = models.CharField()
+    #stock = models.IntegerField()
+    price = models.DecimalField(decimal_places = 2)
+    idCathegory = models.ForeignKey(Cathegory, on_delete=models.CASCADE)
+    #idEditorial = models.ForeignKey(Editorial, on_delete = models.CASCADE)
+
+class CartBook(models.Model):
+    idCart = models.ForeignKey(ShoppingCart, on_delete = models.CASCADE)
+    idBook = models.ForeignKey(Book, on_delete=models.CASCADE)
 
 class Sale(models.Model):
     CREDIT_CARD = "CC"
@@ -31,3 +40,6 @@ class Sale(models.Model):
         choices =  PAY_METHOD_CHOICES,
         default = CREDIT_CARD,
     )
+    idCart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE)
+    total = models.ForeignKey(ShoppingCart, to_field='total', on_delete=models.CASCADE)
+    date = models.DateField(auto_now=True)
