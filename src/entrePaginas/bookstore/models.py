4d3716470f6c60c61d31705d7f24be2de84d987d
mramirezcc <mramirezcc@unsa.edu.pe>
2024-07-01 21:32:45 -0500

    Primeras definiciones de los modelos

--- src/entrePaginas/bookstore/models.py
+++ src/entrePaginas/bookstore/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 
-# Create your models here.
-class User(models.Model){
-    
-}
+class User(models.Model):
+    name = models.CharField()
+    email = models.EmailField()
+    password = models.CharField()
+    address = models.CharField()
+
+class ShoppingCart(models.Model):
+    total = models.IntegerField()
+
+class CartBook(models.Model):
+    idCart = models.ForeignKey(ShoppingCart, on_delete = models.CASCADE)
+
+class Book(models.Model):
+    name = models.CharField()
+
+class Cathegory(models.Model):
+    name = models.CharField()
+
+class Sale(models.Model):
+    CREDIT_CARD = "CC"
+    DEBIT_CARD = "DC"
+    CASH = "$"
+    PAY_METHOD_CHOICES = {
+        CREDIT_CARD: 'Credit card',
+        DEBIT_CARD: 'Debit card',
+        CASH: 'Cash',
+    }
+    payMethod = models.CharField(
+        choices =  PAY_METHOD_CHOICES,
+        default = CREDIT_CARD,
+    )
