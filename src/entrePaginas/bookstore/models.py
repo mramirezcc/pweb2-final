0d6b59095d0549ac66c80e7a08fb0c58dbc8e784
mramirezcc <mramirezcc@unsa.edu.pe>
2024-07-01 22:21:27 -0500

    Modelos corregidos, migraciones funcionales

--- src/entrePaginas/bookstore/models.py
+++ src/entrePaginas/bookstore/models.py
@@ -1,31 +1,31 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     email = models.EmailField()
-    password = models.CharField()
-    address = models.CharField()
+    password = models.CharField(max_length=8)
+    address = models.CharField(max_length=100)
 
 class ShoppingCart(models.Model):
-    idUser = models.ForeignKey(User, on_delete = models.CASCADE)
+    idUser = models.ForeignKey(User, on_delete = models.CASCADE, related_name='idUser_cart')
     total = models.IntegerField()
     status = models.BooleanField(default = False)
 
 class Cathegory(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=20)
 
 class Book(models.Model):
-    name = models.CharField()
-    year = models.IntegerField(max_length = 4)
-    author = models.CharField()
+    name = models.CharField(max_length=50)
+    year = models.IntegerField()
+    author = models.CharField(max_length=100)
     #stock = models.IntegerField()
-    price = models.DecimalField(decimal_places = 2)
-    idCathegory = models.ForeignKey(Cathegory, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places = 2)
+    idCathegory = models.ForeignKey(Cathegory, on_delete=models.CASCADE, related_name='cathegory_book')
     #idEditorial = models.ForeignKey(Editorial, on_delete = models.CASCADE)
 
 class CartBook(models.Model):
-    idCart = models.ForeignKey(ShoppingCart, on_delete = models.CASCADE)
-    idBook = models.ForeignKey(Book, on_delete=models.CASCADE)
+    idCart = models.ForeignKey(ShoppingCart, on_delete = models.CASCADE, related_name='idCart_cartbook')
+    idBook = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='idBook_cartbook')
 
 class Sale(models.Model):
     CREDIT_CARD = "CC"
@@ -37,9 +37,10 @@ class Sale(models.Model):
         CASH: 'Cash',
     }
     payMethod = models.CharField(
+        max_length=20,
         choices =  PAY_METHOD_CHOICES,
         default = CREDIT_CARD,
     )
-    idCart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE)
-    total = models.ForeignKey(ShoppingCart, to_field='total', on_delete=models.CASCADE)
+    idCart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE, related_name='idCart_sale')
+    total = models.IntegerField()
     date = models.DateField(auto_now=True)
