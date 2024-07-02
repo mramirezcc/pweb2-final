0b27aafe083dc403e3fa0a8087b84071f2451d05
mramirezcc <mramirezcc@unsa.edu.pe>
2024-07-01 22:00:48 -0500

    cambios menores

--- src/entrePaginas/bookstore/admin.py
+++ src/entrePaginas/bookstore/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, ShoppingCart, Cathegory, Book, CartBook, Sale
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(ShoppingCart)
+admin.site.register(Cathegory)
+admin.site.register(Book)
+admin.site.register(CartBook)
+admin.site.register(Sale)
