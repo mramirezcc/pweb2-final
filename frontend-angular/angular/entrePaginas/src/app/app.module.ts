1e615269b2fd8c101546f223cf9f2bdfe0bf67dd
dev1d123 <dhuamanio@unsa.edu.pe>
2024-07-17 03:02:57 -0500

    Se añadieron las implementaciones iniciales de los formularios para el registro e iniciar sesión

--- frontend-angular/angular/entrePaginas/src/app/app.module.ts
+++ frontend-angular/angular/entrePaginas/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
