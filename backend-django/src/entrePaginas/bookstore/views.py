59120e909862a485582c55d0be0a91d90a847550
mramirezcc <mramirezcc@unsa.edu.pe>
2024-07-18 11:50:20 -0500

    Form de login

--- backend-django/src/entrePaginas/bookstore/views.py
+++ backend-django/src/entrePaginas/bookstore/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from bookstore.models import User
-from .forms import RegisterForm
+from django.shortcuts import render, redirect
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import login, authenticate
 
 def userRegisterView(request):
     myForm = RegisterForm(request.POST or None)
@@ -12,3 +12,18 @@ def userRegisterView(request):
         'form': myForm
     }
     return render(request, 'bookstore/test.html', context)
+
+def loginView(request):
+    if request.method == 'POST':
+        myForm = LoginForm(data=request.POST)
+        if myForm.is_valid():
+            user = authenticate(request, name=myForm.cleaned_data.get('name'), password=myForm.cleaned_data.get('password'))
+            login(request, user)
+            return redirect('homepage')
+    else:
+        myForm = LoginForm()
+
+    context = {
+        'form': myForm
+    }
+    return render(request, 'bookstore/login.html', context)
